d177cc8e2236c51ac79988b8f03240512f2100a4
Otabek Mirzakhmedov <Otabek.Mirzakhmedov@gmail.com>
2023-08-02 19:40:26 +0500

    fix: csv reading data consequence is fixed

--- Data/User.cs
+++ Data/User.cs
@@ -4,9 +4,9 @@ namespace VentionTask.Data
 {
     public class User
     {
+        public string UserName { get; set; }
         [Key]
         public string UserIdentifier { get; set; }
-        public string UserName { get; set; }
         public int Age { get; set; }
         public string City { get; set; }
         public string PhoneNumber { get; set; }
