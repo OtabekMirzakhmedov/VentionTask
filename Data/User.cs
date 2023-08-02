06e1bad3b5e9798d3a0bbf66bebcb89b95bcefe6
User <User@DESKTOP-C51DDIB>
2023-08-02 15:15:51 +0500

    init: user model created

---
+++ Data/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VentionTask.Data
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string UserIdentifier { get; set;}
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
